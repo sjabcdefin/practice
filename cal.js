8f429f1b0f05a8cd44098c87a5d55ce6a717a28b
tomoka <tomoka.saiwaki@gmail.com>
2024-12-20 16:53:23 +0900

    Update: Modify
    いろいろ修正

--- cal.js
+++ cal.js
@@ -9,22 +9,28 @@ const month = params.m ?? today.getMonth() + 1;
 const year = params.y ?? today.getFullYear();
 
 const startDate = new Date(year, month - 1);
-
-const startDate = new Date(year, month);
 const endDate = new Date(year, startDate.getMonth() + 1, 0);
 
-console.log(`      ${month}月 ${year}     `);
+console.log(`      ${month}月 ${year}`);
 console.log("日 月 火 水 木 金 土");
-process.stdout.write(" ".repeat(3 * startDate.getDay()));
+process.stdout.write("   ".repeat(startDate.getDay()));
+
+let numberOfLines = 0;
 
 for (
   const date = new Date(startDate);
   date <= endDate;
   date.setDate(date.getDate() + 1)
 ) {
-  process.stdout.write(`${String(date.getDate()).padStart(2, " ")} `);
+  process.stdout.write(`${String(date.getDate()).padStart(2, " ")}`);
   if (date.getDay() === 6) {
     process.stdout.write("\n");
+    numberOfLines++;
+  } else {
+    process.stdout.write(" ");
   }
 }
-process.stdout.write("\n\n");
+
+for (let lines = numberOfLines; lines < 6; lines++) {
+  process.stdout.write("\n");
+}
