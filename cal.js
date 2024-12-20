1e6037cae1c52b0755f10b0377dbb5b065d9a66d
tomoka <tomoka.saiwaki@gmail.com>
2024-12-20 16:44:06 +0900

    Update: Modify the indicated items.
    指摘事項を修正

--- cal.js
+++ cal.js
@@ -3,25 +3,32 @@
 import minimist from "minimist";
 
 const params = minimist(process.argv.slice(2));
-const month =
-  params["m"] === undefined ? new Date().getMonth() : params["m"] - 1;
-const year = params["y"] === undefined ? new Date().getFullYear() : params["y"];
 
-const start_of_month = new Date(year, month);
-const end_of_month = new Date(year, start_of_month.getMonth() + 1, 0);
+const today = new Date();
+const month = params.m ?? today.getMonth() + 1;
+const year = params.y ?? today.getFullYear();
 
-console.log(`      ${month + 1}月 ${year}     `);
+const startDate = new Date(year, month - 1);
+const endDate = new Date(year, startDate.getMonth() + 1, 0);
+
+console.log(`      ${month}月 ${year}`);
 console.log("日 月 火 水 木 金 土");
-process.stdout.write(" ".repeat(3 * start_of_month.getDay()));
+process.stdout.write("   ".repeat(startDate.getDay()));
 
 for (
-  const day = new Date(start_of_month);
-  day <= end_of_month;
-  day.setDate(day.getDate() + 1)
+  const date = new Date(startDate);
+  date <= endDate;
+  date.setDate(date.getDate() + 1)
 ) {
-  process.stdout.write(`${String(day.getDate()).padStart(2, " ")} `);
-  if (day.getDay() === 6) {
+  process.stdout.write(`${String(date.getDate()).padStart(2, " ")}`);
+  if (date.getDay() === 6) {
     process.stdout.write("\n");
+  } else {
+    process.stdout.write(" ");
   }
 }
-process.stdout.write("\n\n");
+
+if (endDate.getDay() !== 6) {
+  process.stdout.write("\n");
+}
+process.stdout.write("\n");
