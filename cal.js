75db55b2eebefb43e1358f0459477e3c887aef05
tomoka <tomoka.saiwaki@gmail.com>
2024-12-20 16:51:58 +0900

    Update: Modify the variable name of Date object.
    Dateオブジェクトが格納される変数名を変更。
    - 変数(startDate, endDate)について、キャメルケースを使用するように修正
    - 変数名start_of_monthをstartDate, end_of_monthをendDate, dayをdateに修正

--- cal.js
+++ cal.js
@@ -8,22 +8,22 @@ const today = new Date();
 const month = params.m ?? today.getMonth() + 1;
 const year = params.y ?? today.getFullYear();
 
-const start_of_month = new Date(year, month - 1);
+const startDate = new Date(year, month - 1);
 
-const start_of_month = new Date(year, month);
-const end_of_month = new Date(year, start_of_month.getMonth() + 1, 0);
+const startDate = new Date(year, month);
+const endDate = new Date(year, startDate.getMonth() + 1, 0);
 
 console.log(`      ${month}月 ${year}     `);
 console.log("日 月 火 水 木 金 土");
-process.stdout.write(" ".repeat(3 * start_of_month.getDay()));
+process.stdout.write(" ".repeat(3 * startDate.getDay()));
 
 for (
-  const day = new Date(start_of_month);
-  day <= end_of_month;
-  day.setDate(day.getDate() + 1)
+  const date = new Date(startDate);
+  date <= endDate;
+  date.setDate(date.getDate() + 1)
 ) {
-  process.stdout.write(`${String(day.getDate()).padStart(2, " ")} `);
-  if (day.getDay() === 6) {
+  process.stdout.write(`${String(date.getDate()).padStart(2, " ")} `);
+  if (date.getDay() === 6) {
     process.stdout.write("\n");
   }
 }
