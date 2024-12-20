e40b7c4fe43233430d272d2761311de9e39545c9
tomoka <tomoka.saiwaki@gmail.com>
2024-12-20 16:50:21 +0900

    Update: Modify the method to obtain the year and month to display.
    表示する年、月の取得方法を修正。
    - param["m"],param["y"]について、ドット記法に修正
    - undefinedの場合の判定に、??演算子を使用するように修正
    - 変数month, yearについて、new Date()を変数todayに設定し、todayからmonth, yearをそれぞれ取得するように修正

--- cal.js
+++ cal.js
@@ -8,27 +8,23 @@ const today = new Date();
 const month = params.m ?? today.getMonth() + 1;
 const year = params.y ?? today.getFullYear();
 
-const startDate = new Date(year, month - 1);
-const endDate = new Date(year, startDate.getMonth() + 1, 0);
+const start_of_month = new Date(year, month - 1);
 
-console.log(`      ${month}月 ${year}`);
+const start_of_month = new Date(year, month);
+const end_of_month = new Date(year, start_of_month.getMonth() + 1, 0);
+
+console.log(`      ${month}月 ${year}     `);
 console.log("日 月 火 水 木 金 土");
-process.stdout.write("   ".repeat(startDate.getDay()));
+process.stdout.write(" ".repeat(3 * start_of_month.getDay()));
 
 for (
-  const date = new Date(startDate);
-  date <= endDate;
-  date.setDate(date.getDate() + 1)
+  const day = new Date(start_of_month);
+  day <= end_of_month;
+  day.setDate(day.getDate() + 1)
 ) {
-  process.stdout.write(`${String(date.getDate()).padStart(2, " ")}`);
-  if (date.getDay() === 6) {
+  process.stdout.write(`${String(day.getDate()).padStart(2, " ")} `);
+  if (day.getDay() === 6) {
     process.stdout.write("\n");
-  } else {
-    process.stdout.write(" ");
   }
 }
-
-if (endDate.getDay() !== 6) {
-  process.stdout.write("\n");
-}
-process.stdout.write("\n");
+process.stdout.write("\n\n");
